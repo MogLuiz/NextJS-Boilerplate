44eaef74f523041ce0b954c23082f43133531952
Luiz Henrique <luizhjramos@outlook.com>
2021-11-04 16:26:56 -0300

    test: snapshots case test

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -4,10 +4,12 @@ import Main from "."
 
 describe("<Main />", () => {
   it("should the render header", () => {
-    render(<Main />)
+    const { container } = render(<Main />)
 
     expect(
       screen.getByRole("heading", { name: /react avançado/i })
     ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
