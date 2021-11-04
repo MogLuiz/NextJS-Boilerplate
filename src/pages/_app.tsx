eba0d5f90bc3fdc72b34a1106ae95a30378cf976
Luiz Henrique <luizhjramos@outlook.com>
2021-11-04 18:41:37 -0300

    feat: absolut imports config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@
 import type { AppProps } from "next/app"
 import { Head } from "next/document"
 
-import GlobalStyles from "../styles/global"
+import GlobalStyles from "styles/global"
 
 function App({ Component, pageProps }: AppProps) {
   return (
