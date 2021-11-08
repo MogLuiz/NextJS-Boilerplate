f61a05c59b01903583860631674a8dacfdeeb926
Luiz Henrique <luizhjramos@outlook.com>
2021-11-08 10:54:26 -0300

    feat: stories remove

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -19,21 +19,3 @@ describe("<Main />", () => {
     expect(container.firstChild).toHaveStyle({ "background-color": "#06092b" })
   })
 })
-
-describe("<Main />", () => {
-  it("should render the heading", () => {
-    const { container } = render(<Main />)
-
-    expect(
-      screen.getByRole("heading", { name: /react avançado/i })
-    ).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
-  })
-
-  it("should render the colors correctly", () => {
-    const { container } = render(<Main />)
-
-    expect(container.firstChild).toHaveStyle({ "background-color": "#06092b" })
-  })
-})
