ad41609d2fd79a6af50b91527e455434eb22c8c6
Luiz Henrique <luizhjramos@outlook.com>
2021-11-09 09:56:54 -0300

    storybook: updating setting for six version

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,17 @@
-import { addDecorator } from "@storybook/react"
-import withGlobalStyles from "./withGlobalStyle"
+import { RouterContext } from 'next/dist/shared/lib/router-context'
+import GlobalStyles from '../src/styles/global'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+  nextRouter: {
+    Provider: RouterContext.Provider
+  }
 }
 
-addDecorator(withGlobalStyles)
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyles />
+      <Story />
+    </>
+  )
+]
