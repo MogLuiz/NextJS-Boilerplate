ffe9976d076140a14975717335e71c80fd6d5a36
Luiz Henrique <luizhjramos@outlook.com>
2021-11-09 11:24:26 -0300

    storybook: Essentials and controls config

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,8 +4,6 @@ module.exports = {
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
-    "@storybook/addon-actions",
-    "@storybook/addon-links",
-    "@storybook/addon-knobs/register"
+    "@storybook/addon-essentials",
   ]
 }
